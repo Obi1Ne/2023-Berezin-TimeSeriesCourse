79365da2be59745f6ef27b57d4600d76c9239d54
Иван Березин <116001313+Obi1Ne@users.noreply.github.com>
2023-09-14 18:18:22 +0500

    Add files via upload

--- practice/01 Basics/modules/metrics.py
+++ practice/01 Basics/modules/metrics.py
@@ -2,27 +2,9 @@ import numpy as np
 
 
 def ED_distance(ts1, ts2):
-    """
-    Calculate the Euclidean distance.
-
-    Parameters
-    ----------
-    ts1 : numpy.ndarray
-        The first time series.
-
-    ts2 : numpy.ndarray
-        The second time series.
-
-    Returns
-    -------
-    ed_dist : float
-        Euclidean distance between ts1 and ts2.
-    """
-    
-    ed_dist = 0
-
-    # INSERT YOUR CODE
-    
+    squared_diff = (ts1 - ts2) ** 2
+    sum_squared_diff = np.sum(squared_diff)
+    ed_dist = np.sqrt(sum_squared_diff) 
     return ed_dist
 
 
@@ -52,28 +34,20 @@ def norm_ED_distance(ts1, ts2):
 
 
 def DTW_distance(ts1, ts2, r=None):
-    """
-    Calculate DTW distance.
-
-    Parameters
-    ----------
-    ts1 : numpy.ndarray
-        The first time series.
-
-    ts2 : numpy.ndarray
-        The second time series.
-
-    r : float
-        Warping window size.
     
-    Returns
-    -------
-    dtw_dist : float
-        DTW distance between ts1 and ts2.
-    """
+    n = len(ts1)
 
-    dtw_dist = 0
+    cost_matrix = np.full((n, n), np.inf)
+    cost_matrix[0, 0] = (ts1[0] - ts2[0]) ** 2
+    
+    if r is None:
+        r = max(n, n)
 
-    # INSERT YOUR CODE
-
+    for i in range(1, n):
+        for j in range(max(1, i - r), min(n, i + r)):
+            cost = (ts1[i] - ts2[j]) ** 2
+            cost_matrix[i, j] = cost + min(cost_matrix[i - 1, j], cost_matrix[i, j - 1], cost_matrix[i - 1, j - 1])
+ 
+    dtw_dist = cost_matrix[n - 1, n - 1]
+    
     return dtw_dist
